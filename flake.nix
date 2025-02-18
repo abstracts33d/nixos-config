8eb03871b5cfad3e2a42d6fc854eb9d1f5191721
abstracts33d <abstract.s33d@gmail.com>
2025-02-18 23:17:36 +0100

    remove change

--- flake.nix
+++ flake.nix
@@ -112,12 +112,6 @@
               useGlobalPkgs = true;
               useUserPackages = true;
               users.${user} = import ./modules/nixos/home-manager-bkp.nix;
-              #users.${user} = { ... }: {
-              #  imports = [
-              #    ./modules/shared/home-manager.nix
-              #    ./modules/nixos/home-manager.nix
-              #  ];
-              #};
             };
           }
           ./hosts/nixos
