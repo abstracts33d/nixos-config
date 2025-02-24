57d17065f6b180cef9d9261828a7aa764f464f09
abstracts33d <abstract.s33d@gmail.com>
2025-02-24 21:44:13 +0100

    test update

--- modules/nixos/home-manager.nix
+++ modules/nixos/home-manager.nix
@@ -6,18 +6,18 @@ let
 in
 {
   home-manager = {
-    home = {
-      enableNixpkgsReleaseCheck = false;
-      username = "${user}";
-      homeDirectory = "/home/${user}";
-      packages = pkgs.callPackage ./packages.nix {};
-      file = shared-files // import ./files.nix { inherit user config pkgs; };
-      stateVersion = "21.05";
-    };
     useGlobalPkgs = true;
     useUserPackages = true;
     backupFileExtension = "_nbkp";
     users.${user} = { ... }: {
+      home = {
+        enableNixpkgsReleaseCheck = false;
+        username = "${user}";
+        homeDirectory = "/home/${user}";
+        packages = pkgs.callPackage ./packages.nix {};
+        file = shared-files // import ./files.nix { inherit user config pkgs; };
+        stateVersion = "21.05";
+      };
       imports = [
         ../shared/home-manager.nix
         ./hm/gtk.nix
