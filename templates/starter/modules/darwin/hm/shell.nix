39c16de1ce56f834f8f48320839ad54706c16bc4
abstracts33d <abstract.s33d@gmail.com>
2025-03-02 12:35:07 +0100

    add forgotten files

---
+++ templates/starter/modules/darwin/hm/shell.nix
@@ -0,0 +1,8 @@
+{ config, pkgs, lib, ... }:
+
+let name = "abstracts33d";
+    user = "s33d";
+    email = "abstract.s33d@gmail.com"; in
+{
+
+}
---
+++ templates/starter/modules/darwin/local.nix
@@ -0,0 +1,42 @@
+{ config, pkgs, ... }:
+
+let
+  user = "s33d";
+in
+{
+  # Fully declarative dock using the latest from Nix Store
+  local = {
+    dock = {
+      enable = true;
+      entries = [
+        { path = "/Applications/Slack.app/"; }
+        { path = "/Applications/Notion.app/"; }
+        { path = "/Applications/Obsidian.app/"; }
+        { path = "${pkgs.alacritty}/Applications/Alacritty.app/"; }
+        { path = "/Applications/Kitty.app/"; }
+        { path = "/Applications/Visual Studio Code.app/"; }
+        { path = "/Applications/RubyMine.app/"; }
+        { path = "/Applications/Safari.app/"; }
+        { path = "/Applications/Google Chrome.app/"; }
+        { path = "/Applications/Zen Browser.app/"; }
+        { path = "/System/Applications/System Settings.app/"; }
+        { path = "/Applications/UTM.app/"; }
+        {
+          path = "${config.users.users.${user}.home}/.config/";
+          section = "others";
+          options = "--sort name --view grid --display folder";
+        }
+        {
+          path = "${config.users.users.${user}.home}/.local/share/";
+          section = "others";
+          options = "--sort name --view grid --display folder";
+        }
+        {
+          path = "${config.users.users.${user}.home}/Downloads";
+          section = "others";
+          options = "--sort name --view grid --display stack";
+        }
+      ];
+    };
+  };
+}
