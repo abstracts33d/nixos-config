6e1d5b19677cb85909112f6c6b210d5bd4d594ee
abstracts33d <abstract.s33d@gmail.com>
2025-03-04 18:32:54 +0100

    test update commit FIX

--- hosts/darwin/aether/default.nix
+++ hosts/darwin/aether/default.nix
@@ -1,4 +1,4 @@
-{ inputs, config, inputs, pkgs, agenix, ... }:
+{ inputs, config, pkgs, agenix, ... }:
 {
   imports = [
     "../darwin"
--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -1,4 +1,4 @@
-{ inputs, config, inputs, pkgs, agenix, ... }:
+{ inputs, config, pkgs, agenix, ... }:
 
 let user = config.hostSpec.username;
     keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOk8iAnIaa1deoc7jw8YACPNVka1ZFJxhnU4G74TmS+p" ]; in
