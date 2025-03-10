cd63ee637208a527b917a6ecb57a37e6fcfc250a
abstracts33d <abstract.s33d@gmail.com>
2025-03-11 00:49:56 +0100

    update stylix flake home manager

--- flake.nix
+++ flake.nix
@@ -3,12 +3,15 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     agenix.url = "github:ryantm/agenix";
-    # home-manager.url = "github:nix-community/home-manager";
     home-manager = {
-         url = "github:nix-community/home-manager";
-         inputs.nixpkgs.follows = "nixpkgs";
-     };
-    stylix.url = "github:danth/stylix";
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    stylix = {
+      url = "github:danth/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
     darwin = {
       url = "github:LnL7/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
