9c8ef6442f83a0582a239e2137d99b3229ca3a19
abstracts33d <abstract.s33d@gmail.com>
2025-03-11 00:18:24 +0100

    update stylix

--- modules/nixos/config/nix/theme.nix
+++ modules/nixos/config/nix/theme.nix
@@ -1,4 +1,4 @@
-{ pkgs, }:
+{ pkgs, inputs, ... }:
 {
   # If stylix.base16Scheme is undeclared, Stylix generates a color scheme based on the wallpaper
   # inputs.stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
