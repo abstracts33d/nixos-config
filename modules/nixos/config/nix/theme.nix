6b2433a8258ce36fdd6ef5d0c18bf3341a3f4fc8
abstracts33d <abstract.s33d@gmail.com>
2025-03-11 00:20:05 +0100

    update stylix

--- modules/nixos/config/nix/theme.nix
+++ modules/nixos/config/nix/theme.nix
@@ -1,16 +1,16 @@
-{ pkgs, inputs, ... }:
+{ pkgs, }:
 {
   # If stylix.base16Scheme is undeclared, Stylix generates a color scheme based on the wallpaper
-  # inputs.stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
+  # stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
 
-  inputs.stylix.image = pkgs.fetchurl {
+  stylix.image = pkgs.fetchurl {
     url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
     sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
   };
 
-  inputs.stylix.polarity = "dark";
+  stylix.polarity = "dark";
 
-  inputs.stylix.fonts = {
+  stylix.fonts = {
     serif = {
       package = pkgs.dejavu_fonts;
       name = "DejaVu Serif";
@@ -31,6 +31,4 @@
       name = "Noto Color Emoji";
     };
   };
-
-  inputs.stylix.targets.vscode.enable = false;
 }
