47e60864de66fc36f6a59329d2acb672e736840e
abstracts33d <abstract.s33d@gmail.com>
2025-03-10 08:54:12 +0100

    add iso with spice and qemuGuest

---
+++ iso/flake.nix
@@ -0,0 +1,14 @@
+{
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.live = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        (nixpkgs + "/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix")
+        ./additional-config.nix
+      ];
+    };
+  };
+}
+#nix build .#nixosConfigurations.live.config.system.build.isoImage
