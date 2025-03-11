50949b73daa2bd1bbb8fd0146ea74a2736195410
abstracts33d <abstract.s33d@gmail.com>
2025-03-11 22:29:24 +0100

    update

---
+++ modules/nixos/optional/desktop/hyprland/ssdm.nix
@@ -0,0 +1,30 @@
+{ config, lib, ... }:
+
+{
+  config = lib.mkIf (config.hyprland.enable) {
+    services = {
+      displayManager = {
+        sddm = {
+          enable = true;
+          package = pkgs.kdePackages.sddm;
+          wayland = {
+            enable = true;
+          };
+          settings = {
+            Wayland = {
+              SessionDir = "${inputs.hyprland.packages.${pkgs.system}.hyprland}/share/wayland-sessions";
+            };
+          };
+#          catppuccin = {
+#            enable = true;
+#            assertQt6Sddm = true;
+#            flavor = "macchiato";
+#            font = "0xProto Nerd Font";
+#            fontSize = "12";
+#            loginBackground = true;
+#          };
+        };
+      };
+    };
+  };
+}
