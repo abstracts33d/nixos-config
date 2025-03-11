b85a5881663308da62646b8459b1c4c4ee2e87e4
abstracts33d <abstract.s33d@gmail.com>
2025-03-11 12:07:24 +0100

    add gpg-agent.conf

--- modules/shared/home-manager/gnupg.nix
+++ modules/shared/home-manager/gnupg.nix
@@ -5,19 +5,7 @@
     gnupg = {
       agent = {
         enable = true;
-        enableSSHSupport = true;
-        pinentryFlavor = "curses";
       };
     };
   };
 }
-
-
-
-pinentry-program /usr/bin/pinentry-tty
-default-cache-ttl 34560000
-max-cache-ttl 34560000
-
-pinentry-program /opt/homebrew/bin/pinentry-mac
-default-cache-ttl 34560000
-max-cache-ttl 34560000
