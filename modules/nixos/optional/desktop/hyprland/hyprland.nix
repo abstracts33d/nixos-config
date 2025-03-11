97bf9830a653a20456ab4fd69aa44e66c0fc69a4
abstracts33d <abstract.s33d@gmail.com>
2025-03-11 20:57:06 +0100

    try fix google-chrome

--- modules/nixos/optional/desktop/hyprland/hyprland.nix
+++ modules/nixos/optional/desktop/hyprland/hyprland.nix
@@ -29,21 +29,6 @@ in
     home-manager.users.${user} = {
       wayland.windowManager.hyprland = {
         enable = true;
-        systemd = {
-          enable = true;
-          variables = [ "--all" ]; # fix for https://wiki.hyprland.org/Nix/Hyprland-on-Home-Manager/#programs-dont-work-in-systemd-services-but-do-on-the-terminal
-          # Same as default, but stop the graphical session too
-          extraCommands = lib.mkBefore [
-            "systemctl --user stop graphical-session.target"
-            "systemctl --user start hyprland-session.target"
-          ];
-        };
-
-        # plugins = [
-        #   pkgs.hyprlandPlugins.hyperbars
-        # ];
-
-        # settings = {};
       };
     };
   };
