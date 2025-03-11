c4c269f46f81a04d2088f31d990fb9709b420971
abstracts33d <abstract.s33d@gmail.com>
2025-03-11 21:31:32 +0100

    update

--- modules/nixos/optional/desktop/hyprland/hyprland.nix
+++ modules/nixos/optional/desktop/hyprland/hyprland.nix
@@ -30,6 +30,30 @@ in
     home-manager.users.${user} = {
       wayland.windowManager.hyprland = {
         enable = true;
+        settings = {
+          "$mod" = "SUPER";
+
+          "$terminal" = "kitty";
+          "$browser" = "firefox";
+          "$launcher" = "tofi-drun -c ~/.config/tofi/tofi.launcher.conf";
+          "$launcher_alt" = "tofi-run -c ~/.config/tofi/tofi.launcher.conf";
+          "$launcher2" = "wofi --show drun -n";
+          "$launcher2_alt" = "wofi --show run -n";
+          "$editor" = "code";
+
+
+          bind = [
+            # General
+            "$mod, return, exec, $terminal"
+            "$mod, a, exec, $launcher"
+            "$mod, s, exec, $launcher_alt"
+            "$mod, d, exec, $launcher2"
+            "$mod, f, exec, $launcher2_alt"
+            "$mod SHIFT, q, killactive"
+            "$mod SHIFT, e, exit"
+            "$mod SHIFT, l, exec, ${pkgs.hyprlock}/bin/hyprlock"
+          ];
+        };
       };
     };
   };
