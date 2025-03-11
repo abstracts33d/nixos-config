510f671434ff967032afc388f43e44942258e958
abstracts33d <abstract.s33d@gmail.com>
2025-03-11 09:53:43 +0100

    update kitty.nix

--- modules/shared/home-manager/kitty.nix
+++ modules/shared/home-manager/kitty.nix
@@ -4,10 +4,8 @@
   programs = {
     kitty = {
       enable = true;
-      # Handled by the theme
-
+      # Handled by the theme module
       # theme = "Catppuccin-Macchiato";
-
       # font = {
       #  name = "MesloLG NF";
       #  package = pkgs.nerd-fonts.meslo-lg;
