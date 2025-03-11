d4501bf142a8fa1ea0e3136177c3b29a770a4897
abstracts33d <abstract.s33d@gmail.com>
2025-03-11 16:53:59 +0100

    update stylix.nix

--- modules/nixos/config/nix/theme.nix
+++ modules/nixos/config/nix/theme.nix
@@ -1,38 +1,38 @@
 { pkgs, }:
 {
-  # If stylix.base16Scheme is undeclared, Stylix generates a color scheme based on the wallpaper
-   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
+  stylix = {
+    enable = true;
+    # autoEnable = false;
 
-  stylix.enable = true;
+    # If stylix.base16Scheme is undeclared, Stylix generates a color scheme based on the wallpaper
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
+    image = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/orangci/walls-catppuccin-mocha/refs/heads/master/cool.jpg";
+      sha256 = "+gmixlxI+gTrWKq5Gnb7yaj/V0JWIO6dlk6uESeJ3ho=";
+    };
 
-  stylix.image = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/orangci/walls-catppuccin-mocha/refs/heads/master/cool.jpg";
-    sha256 = "+gmixlxI+gTrWKq5Gnb7yaj/V0JWIO6dlk6uESeJ3ho=";
+    polarity = "dark";
+
+    fonts = {
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans";
+      };
+
+      monospace = {
+        package = pkgs.nerd-fonts.meslo-lg;
+        name = "MesloLG NF";
+      };
+
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+    };
   };
-
-  stylix.polarity = "dark";
-
-  stylix.fonts = {
-    serif = {
-      package = pkgs.dejavu_fonts;
-      name = "DejaVu Serif";
-    };
-
-    sansSerif = {
-      package = pkgs.dejavu_fonts;
-      name = "DejaVu Sans";
-    };
-
-    monospace = {
-      package = pkgs.nerd-fonts.meslo-lg;
-      name = "MesloLG NF";
-    };
-
-    emoji = {
-      package = pkgs.noto-fonts-emoji;
-      name = "Noto Color Emoji";
-    };
-  };
-
-  # stylix.autoEnable = false;
 }
