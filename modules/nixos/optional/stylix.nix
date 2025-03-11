4e405890c0386aee5fb131feb17f58430b94c550
abstracts33d <abstract.s33d@gmail.com>
2025-03-11 23:41:21 +0100

    update theming

--- modules/nixos/optional/stylix.nix
+++ modules/nixos/optional/stylix.nix
@@ -17,9 +17,46 @@ in
     };
   };
 
-  config = lib.mkIf (config.theme.enable) theme;
+#  config = lib.mkIf (config.theme.enable) theme;
 
-#  config = lib.mkIf (config.theme.enable) {
+  config = lib.mkIf (config.theme.enable) {
+    stylix = {
+      enable = true;
+      # autoEnable = false;
+
+      # If stylix.base16Scheme is undeclared, Stylix generates a color scheme based on the wallpaper
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
+
+      image = pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/orangci/walls-catppuccin-mocha/refs/heads/master/cool.jpg";
+        sha256 = "+gmixlxI+gTrWKq5Gnb7yaj/V0JWIO6dlk6uESeJ3ho=";
+      };
+
+      polarity = "dark";
+
+      fonts = {
+        serif = {
+          package = pkgs.dejavu_fonts;
+          name = "DejaVu Serif";
+        };
+
+        sansSerif = {
+          package = pkgs.dejavu_fonts;
+          name = "DejaVu Sans";
+        };
+
+        monospace = {
+          package = pkgs.nerd-fonts.meslo-lg;
+          name = "MesloLG NF";
+        };
+
+        emoji = {
+          package = pkgs.noto-fonts-emoji;
+          name = "Noto Color Emoji";
+        };
+      };
+    };
+
     home-manager.users.${user} = {
       stylix = {
         targets = {
@@ -33,5 +70,5 @@ in
         };
       };
     };
-#  };
+  };
 }
