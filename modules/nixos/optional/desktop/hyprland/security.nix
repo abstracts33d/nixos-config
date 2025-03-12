1bb446f52c9aa49c28ecf61ec852e3194edc2d15
abstracts33d <abstract.s33d@gmail.com>
2025-03-12 21:06:52 +0100

    gdm

--- modules/nixos/optional/desktop/hyprland/security.nix
+++ modules/nixos/optional/desktop/hyprland/security.nix
@@ -5,19 +5,6 @@
   security.pam.services.gdm.enableGnomeKeyring = true;
   # security.pam.services.greetd.enableGnomeKeyring = true;
   security.polkit.enable = true;
-#  user.services.polkit-gnome-authentication-agent-1 = {
-#    description = "polkit-gnome-authentication-agent-1";
-#    wantedBy = [ "graphical-session.target" ];
-#    wants = [ "graphical-session.target" ];
-#    after = [ "graphical-session.target" ];
-#    serviceConfig = {
-#        Type = "simple";
-#        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-#        Restart = "on-failure";
-#        RestartSec = 1;
-#        TimeoutStopSec = 10;
-#      };
-#  };
 
   environment.systemPackages = with pkgs; [
     seahorse
