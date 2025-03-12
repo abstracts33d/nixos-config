a5e94e9eada62af578426eb2d0da33b6252e8714
abstracts33d <abstract.s33d@gmail.com>
2025-03-12 23:48:04 +0100

    test

--- modules/nixos/optional/desktop/gtk.nix
+++ modules/nixos/optional/desktop/gtk.nix
@@ -4,11 +4,9 @@ let
   user = config.hostSpec.username;
 in
 {
-  config = lib.mkIf (config.hyprland.enable) {
-    home-manager.users.${user} = {
-      gtk = {
-        enable = true;
-      };
+  home-manager.users.${user} = {
+    gtk = {
+      enable = true;
     };
   };
 }
