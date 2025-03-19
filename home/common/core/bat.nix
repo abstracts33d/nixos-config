fafe307e43464eac5888332802900192c5a9ccf7
abstracts33d <abstract.s33d@gmail.com>
2025-03-19 22:35:17 +0100

    update bat and direnv

--- home/common/core/bat.nix
+++ home/common/core/bat.nix
@@ -1,7 +1,20 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.bat = {
     enable = true;
+    config = {
+      # Git modifications and file header (but no grid)
+      style = "changes,header";
+      # theme = "gruvbox-dark";
+    };
+    extraPackages = builtins.attrValues {
+      inherit (pkgs.bat-extras)
+
+        batgrep # search through and highlight files using ripgrep
+        batdiff # Diff a file against the current git index, or display the diff between to files
+        batman # read manpages using bat as the formatter
+        ;
+    };
   };
 }
