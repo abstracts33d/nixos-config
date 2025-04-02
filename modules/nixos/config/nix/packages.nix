716c1fed9885a0251317d9462a62e3bb87924182
abstracts33d <abstract.s33d@gmail.com>
2025-04-02 20:02:09 +0200

    new TEST

--- modules/nixos/config/nix/packages.nix
+++ modules/nixos/config/nix/packages.nix
@@ -4,7 +4,7 @@ with pkgs;
 let
   hS = hostSpec;
   commonPackages = import ../../../common/config/nix/packages.nix { inherit hostSpec pkgs; };
-  hostPackages = import ../../../../hosts/${hS.hostName}/packages.nix { inherit hostSpec pkgs; };
+  hostPackages = import ../../../../hosts/nixos/${hS.hostName}/packages.nix { inherit hostSpec pkgs; };
 in
 commonPackages
 ++ hostPackages
