84a13822abda785cac96e0ae3aaf7ce98c1f2914
abstracts33d <abstract.s33d@gmail.com>
2025-04-03 22:06:06 +0200

    TEST

--- flake.nix
+++ flake.nix
@@ -45,6 +45,10 @@
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    impermanence = {
+      url = "github:nix-community/impermanence";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     secrets = {
       url = "git+ssh://git@github.com/abstracts33d/nix-secrets.git";
       flake = false;
---
+++ modules/nixos/optional/persistence.nix
@@ -0,0 +1,35 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: let
+  hS = config.hostSpec;
+in {
+  config = lib.mkIf (hS.isImpermanent) {
+    environment.persistence."/nix/persist" = {
+      hideMounts = true;
+      directories = [
+        "/var/log"
+        "/var/lib/systemd/coredump"
+        "/etc/NetworkManager/system-connections"
+      ];
+      files = [
+        # machine-id is used by systemd for the journal, if you don't persist this
+        # file you won't be able to easily use journalctl to look at journals for
+        # previous boots.
+        "/etc/machine-id"
+      ];
+      users.will = {
+        directories = [
+          ".local/share/zoxide"
+          "Downloads"
+        ];
+        files = [
+          ".bash_history"
+          ".zsh_history"
+        ];
+      };
+    };
+  };
+}
