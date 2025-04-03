c78420c8395b5ad7267ec9ffa5dd9006f9e19ed3
abstracts33d <abstract.s33d@gmail.com>
2025-04-03 22:35:20 +0200

    TEST

--- modules/nixos/optional/impermanence.nix
+++ modules/nixos/optional/impermanence.nix
@@ -8,7 +8,7 @@
   hS = config.hostSpec;
 in {
   imports = [
-    imputs.impermanence.nixosModules.impermanence;
+    imputs.impermanence.nixosModules.impermanence
   ];
 
   config = lib.mkIf (hS.isImpermanent) {
