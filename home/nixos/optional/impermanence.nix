47a70339f646880c814f9b560148769856dd056b
abstracts33d <abstract.s33d@gmail.com>
2025-04-04 12:40:29 +0200

    TEST

--- home/nixos/optional/impermanence.nix
+++ home/nixos/optional/impermanence.nix
@@ -6,19 +6,21 @@
 }: let
   hS = config.hostSpec;
 in {
-  persistence = {
-    "/persist/${hS.home}" = {
-      defaultDirectoryMethod = "symlink";
-      directories = [
-        "Documents"
-        "Downloads"
-        "Pictures"
-        "Videos"
-      ];
-      files = [
-        ".zsh_history"
-      ];
-      allowOther = true;
+  home = {
+    persistence = {
+      "/persist/${hS.home}" = {
+        defaultDirectoryMethod = "symlink";
+        directories = [
+          "Documents"
+          "Downloads"
+          "Pictures"
+          "Videos"
+        ];
+        files = [
+          ".zsh_history"
+        ];
+        allowOther = true;
+      };
     };
   };
 }
