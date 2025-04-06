7d9a72b8159343c59ec4fde61af3623c9443e828
abstracts33d <abstract.s33d@gmail.com>
2025-04-06 11:14:34 +0200

    Test

--- hosts/nixos/utm/disk-config.nix
+++ hosts/nixos/utm/disk-config.nix
@@ -1,68 +1,10 @@
-{
-  lib,
-  disk ? "/dev/vda",
-  withSwap ? false,
-  swapSize ? 8,
-  ...
-}: {
-  disko.devices = {
-    disk = {
-      disk0 = {
-        type = "disk";
-        device = disk;
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              priority = 1;
-              name = "ESP";
-              start = "1M";
-              end = "512M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = ["defaults"];
-              };
-            };
-            root = {
-              size = "100%";
-              content = {
-                type = "btrfs";
-                extraArgs = ["-f"]; # Override existing partition
-                subvolumes = {
-                  "@root" = {
-                    mountpoint = "/";
-                    mountOptions = [
-                      "compress=zstd"
-                      "noatime"
-                    ];
-                  };
-                  "@persist" = {
-                    mountpoint = "/persist";
-                    mountOptions = [
-                      "compress=zstd"
-                      "noatime"
-                    ];
-                  };
-                  "@nix" = {
-                    mountpoint = "/nix";
-                    mountOptions = [
-                      "compress=zstd"
-                      "noatime"
-                    ];
-                  };
-                  "@swap" = lib.mkIf withSwap {
-                    mountpoint = "/.swapvol";
-                    swap.swapfile.size = "${swapSize}G";
-                  };
-                };
-              };
-            };
-          };
-        };
+_: {
+  imports = [
+    "../common/disks/btrfs-impermanence-disk.nix"
+    {
+      _module.args = {
+        withSwap = false;
       };
-    };
-  };
+    }
+  ];
 }
