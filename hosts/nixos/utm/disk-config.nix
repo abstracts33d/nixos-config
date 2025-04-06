9a31b7885bf340477da94b1f5e008a31fcc374da
abstracts33d <abstract.s33d@gmail.com>
2025-04-06 11:01:24 +0200

    Test

--- hosts/nixos/utm/disk-config.nix
+++ hosts/nixos/utm/disk-config.nix
@@ -1,60 +1,10 @@
 _: {
-  disko.devices = {
-    disk = {
-      disk0 = {
-        type = "disk";
-        device = "/dev/vda";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              priority = 1;
-              name = "ESP";
-              start = "1M";
-              end = "512M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = ["umask=0077"];
-              };
-            };
-            root = {
-              size = "100%";
-              content = {
-                type = "btrfs";
-                extraArgs = ["-L" "root" "-f"];
-                # Subvolumes must set a mountpoint in order to be mounted,
-                # unless their parent is mounted
-                subvolumes = {
-                  "@root" = {
-                    mountpoint = "/";
-                    mountOptions = [
-                      "compress=zstd"
-                      "noatime"
-                    ];
-                  };
-                  "@persist" = {
-                    mountpoint = "/persist";
-                    mountOptions = [
-                      "compress=zstd"
-                      "noatime"
-                    ];
-                  };
-                  "@nix" = {
-                    mountpoint = "/nix";
-                    mountOptions = [
-                      "compress=zstd"
-                      "noatime"
-                    ];
-                  };
-                };
-              };
-            };
-          };
-        };
+  imports = [
+    "../common/disks/btrfs-impermanence-disk.nix"
+    {
+      _module.args = {
+        withSwap = false;
       };
-    };
-  };
+    }
+  ];
 }
